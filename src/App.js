83d125729297984f97638fcfc8e47c47c3b660b9
高倉光 <takakura.hikaru@codeninth.com>
2018-06-08 18:22:45 +0900

    learned why-jsx

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <h1 className="App">
+        Hello World!
+      </h1>
     );
   }
 }
+
+// jsx
+// トランスパイル jsx -> js
+// これでも動くが可読性が低い
+// class App extends Component {
+//   render() {
+//     return (
+//       React.createElement("div",null,"Hello World!!")
+//     );
+//   }
+// }
 
 export default App;
