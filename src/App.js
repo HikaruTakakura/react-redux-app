603aa1368ad4c839095470265d1fe49064e80258
高倉光 <takakura.hikaru@codeninth.com>
2018-06-09 17:19:03 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 // class App extends Component {
 //   render() {
@@ -14,34 +14,70 @@ import PropTypes from 'prop-types';
 //   }
 // }
 
-const App = () => {
-  const cat = "Meow!!"
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 12 },
-    { name: "Nanashi", age: 3 }
-  ]
-  return(
-    <React.Fragment>
-    {
-      profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />
-      })
-    }
-    </React.Fragment>
-  )
+// const App = () => {
+//   const cat = "Meow!!"
+//   const profiles = [
+//     { name: "Taro", age: 10 },
+//     { name: "Hanako", age: 12 },
+//     { name: "Nanashi", age: 3 }
+//   ]
+//   return(
+//     <React.Fragment>
+//     {
+//       profiles.map((profile, index) => {
+//         return <User name={profile.name} age={profile.age} key={index} />
+//       })
+//     }
+//     </React.Fragment>
+//   )
+// }
+
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    // setStateを実行すると、callbackでrender()を実行する
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  handleMinusButton = () => {
+    // setStateを実行すると、callbackでrender()を実行する
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+  
+  render() {
+    console.log(this.state)
+
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={ this.handlePlusButton }>+1</button>
+        <button onClick={ this.handleMinusButton }>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User= (props) => {
-  return (
-    <div>Hi, I am {props.name}, and {props.age} years old.</div>
-  )
-}
+// const User= (props) => {
+//   return (
+//     <div>Hi, I am {props.name}, and {props.age} years old.</div>
+//   )
+// }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+// User.propTypes = {
+//   name: PropTypes.string,
+//   age: PropTypes.number.isRequired
+// }
 
 export default App;
 
